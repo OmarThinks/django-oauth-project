2b370cb1224d7befe1bcaff3b68d95815a5b8160
Omar Magdy <contact.parmag@gmail.com>
2021-06-16 17:40:09 +0200

    settings.py:configured OAUTH2

--- _app/_app/settings.py
+++ _app/_app/settings.py
@@ -122,7 +122,22 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+
+}
+
+
+
+
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
+}
+
+REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
